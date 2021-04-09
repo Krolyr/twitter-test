2f4fe97c010d861b57f31964b087e09da364f003
Alex Butov <al.bu.ex.et@gmail.com>
2021-04-09 18:48:46 +0300

    implement settings screen

--- src/features/Settings/SettingsScreen/index.tsx
+++ src/features/Settings/SettingsScreen/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Content} from 'native-base';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button} from 'native-base';
+import {useNavigation} from '@react-navigation/native';
 
 export function Settings(): JSX.Element {
+  const navigation = useNavigation();
+
+  const logout = () => {
+    navigation.navigate('Login');
+  };
+
   return (
-    <Content>
-      <Text>Settings</Text>
-    </Content>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+      <Text style={styles.name}>Logged in as: Ivan</Text>
+      <Button large full danger onPress={logout} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}> Logout </Text>
+      </Button>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 16,
+    marginVertical: 20,
+  },
+  name: {
+    fontSize: 16,
+    marginVertical: 20,
+  },
+  buttonContainer: {
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
