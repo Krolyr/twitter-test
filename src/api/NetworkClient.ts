4f407bab0bedc45d5d78b0027bfdd6e9ef5ac2bb
Александр <55999739+Krolyr@users.noreply.github.com>
2023-09-26 17:05:47 +0300

    Update NetworkClient.ts

--- src/api/NetworkClient.ts
+++ src/api/NetworkClient.ts
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://api.twitter.com/2/users/';
 
 axios.defaults.headers = {
   Authorization:
-    'Bearer AAAAAAAAAAAAAAAAAAAAAOTFOQEAAAAAEux9nys1PInoF%2F%2BHNh6wX8r6trU%3DjJ6rGVl72iJfHql1tbbiTr1tIsWUgrqWZSbBogGfBisQJ2GZiD',
+    'Bearer ',
 };
 
 export default {
